d5bcc185fffc91227d3b9c6998002cfb75b08925
Mohammad99Azim <103434512+Mohammad99Azim@users.noreply.github.com>
2022-08-12 08:49:12 +0300

    add: test api was added

--- movie_api_v1/serializers.py
+++ movie_api_v1/serializers.py
@@ -1,12 +1,22 @@
+from django.contrib.auth import get_user_model
 from dataclasses import fields
 import imp
 from rest_framework import serializers
 
 from .models import Movie
 
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = get_user_model()
+#         exclude = ['password','last_login','is_staff','date_joined','groups','user_permissions']
+
+
+
 class MovieSerializer(serializers.ModelSerializer):
     owner_name = serializers.CharField(source='owner.username', read_only=True)
+    # owner = UserSerializer()
     class Meta:
         model = Movie
         fields = ['id','owner','owner_name','title','overview','release_date','vote_average','vote_count','data_created_at','data_updated_at']
         # fields = '__all__'
+        
--- movie_api_v1/tests.py
+++ movie_api_v1/tests.py
@@ -1,21 +1,73 @@
-from django.test import TestCase
+from urllib import response
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+# Create your tests here.
+
+
 
 from .models import Movie
 
 
 # Create your tests here.
 
-class MovieTest(TestCase):
+class MovieAPITest(APITestCase):
     @classmethod
     def setUpTestData(cls):
-        test_user = get_user_model().objects.create_user(username='test', password='test1234')
-        test_user.save()
-        test_thing = Movie.objects.create(owner=test_user,title= 'Ariel',overview= 'Taisto Kasurinen is a Finnish coal miner whose father has just committed suicide and who is framed for a crime he did not commit. In jail, he starts to dream about leaving the country and starting a new life. He escapes from prison but things don',release_date='1988-10-21',vote_average=6.768,vote_count=164)
-        test_thing.save()
+        testuser1 = get_user_model().objects.create_user(
+        username = 'testuser1',password ='pass'
+        )
+        testuser1.save()
 
-    def test_thing_model(self):
-        thing = Movie.objects.get(pk=1)
-        self.assertEqual(str(thing.owner) , 'test')
-        self.assertEqual(str(thing.title) , 'Ariel')
-        self.assertEqual(str(thing.overview) , 'Taisto Kasurinen is a Finnish coal miner whose father has just committed suicide and who is framed for a crime he did not commit. In jail, he starts to dream about leaving the country and starting a new life. He escapes from prison but things don')
+        test_movie = Movie.objects.create(owner=testuser1,title= 'Ariel',overview= 'Taisto Kasurinen is a Finnish coal miner whose father has just committed suicide and who is framed for a crime he did not commit. In jail, he starts to dream about leaving the country and starting a new life. He escapes from prison but things don',release_date='1988-10-21',vote_average=6.768,vote_count=164)
+        test_movie.save()
+
+    def test_movie_model(self):
+        movie = Movie.objects.get(pk=1)
+        self.assertEqual(str(movie.owner) , 'testuser1')
+        self.assertEqual(str(movie.title) , 'Ariel')
+        self.assertEqual(str(movie.overview) , 'Taisto Kasurinen is a Finnish coal miner whose father has just committed suicide and who is framed for a crime he did not commit. In jail, he starts to dream about leaving the country and starting a new life. He escapes from prison but things don')
+
+    def test_get_movies_list(self):
+        url = reverse('movies')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        movies = response.data
+        self.assertEqual(len(movies),1)
+        self.assertEqual(movies[0]['title'],'Ariel')
+    
+    def test_get_movie_by_id(self):
+        url = reverse('movie_detail',args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        movie = response.data
+        self.assertEqual(movie['title'],'Ariel')
+
+    def test_create_movie(self):
+        url = reverse('movies')
+        data = {'owner': 1,'title': 'second','overview': 'and starting a new life.','release_date':"1988-10-21",'vote_average':6.768,'vote_count':164}
+        response = self.client.post(url,data)
+        movies = Movie.objects.all()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(movies),2)
+        self.assertEqual(movies.get(id=2).title,'second')
+
+    def test_update_movie(self):
+        url = reverse('movie_detail',args=(1,))
+        data = {'owner': 1,'title': 'updated','overview': 'and starting a new life.','release_date':"1988-10-21",'vote_average':6.768,'vote_count':164}
+        response = self.client.put(url,data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        movie = response.data
+        self.assertEqual(movie['title'],data['title'])
+        self.assertEqual(movie['overview'],data['overview'])
+        self.assertEqual(movie['owner'],data['owner'])
+
+    def test_delete_movie(self):
+        url = reverse('movie_detail',args=(1,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code , status.HTTP_204_NO_CONTENT)
+        movies = Movie.objects.all()
+        self.assertEqual(len(movies),0)
